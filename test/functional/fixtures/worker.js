dd842375af45bdccfb96d8db7dbec2e15fcd6594
Bryan Kendall <bryan@runnable.com>
2015-09-20 09:45:01 -0700

    cleanup, add: functional test

---
+++ test/functional/fixtures/worker.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var EventEmitter = require('events').EventEmitter;
+var ponos = require('../../../');
+var Promise = require('bluebird');
+
+var TaskFatalError = ponos.TaskFatalError;
+
+/**
+ * A simple worker that will publish a message to a queue.
+ * @param {object} job Object describing the job.
+ * @param {string} job.queue Queue on which the message will be published.
+ * @returns {promise} Resolved when the message is put on the queue.
+ */
+module.exports = function (job) {
+  return Promise.resolve()
+    .then(function () {
+      if (!job.queue) { throw new TaskFatalError('queue is required'); }
+      if (!job.message) { throw new TaskFatalError('message is required'); }
+    })
+    .then(function () {
+      module.exports.emitter.emit('task', { data: job.message });
+    })
+    .catch(function (err) {
+      module.exports.emitter.emit('error', err);
+    });
+};
+
+module.exports.emitter = new EventEmitter();
