697a43e9d75b103e4d2bcd0cac91b193981e166c
Bryan Kendall <bryan@runnable.com>
2015-09-21 15:34:46 -0700

    rabbit default tests

--- test/unit/server.js
+++ test/unit/server.js
@@ -38,10 +38,56 @@ describe('Server', function () {
     });
     afterEach(function () { hermes.hermesSingletonFactory.restore(); });
 
+    describe('with rabbitmq env vars', function () {
+      var envs = {
+        HOSTNAME: 'foobar',
+        PORT: 42,
+        USERNAME: 'luke',
+        PASSWORD: 'skywalker'
+      };
+      before(function () {
+        Object.keys(envs).forEach(function (k) {
+          var oldVal = process.env['RABBITMQ_' + k];
+          process.env['RABBITMQ_' + k] = envs[k];
+          envs[k] = oldVal;
+        });
+      });
+      after(function () {
+        Object.keys(envs).forEach(function (k) {
+          process.env['RABBITMQ_' + k] = envs[k];
+        });
+      });
+      it('should create a hermes client with those options', function () {
+        var s = new ponos.Server({ queues: Object.keys(tasks) });
+        assert.ok(s);
+        var hermesOpts = hermes.hermesSingletonFactory.firstCall.args[0];
+        assert.deepEqual(hermesOpts, {
+          hostname: 'foobar',
+          port: 42,
+          username: 'luke',
+          password: 'skywalker',
+          queues: [ 'test-queue-01', 'test-queue-02' ]
+        });
+      });
+    });
+
     it('should take a Hermes client', function () {
       var s = new ponos.Server({ hermes: noop });
       assert.ok(s);
       assert.equal(s.hermes, noop);
+    });
+    it('should default rabbit mq vars', function () {
+      var s = new ponos.Server({ queues: Object.keys(tasks) });
+      assert.ok(s);
+      assert.equal(s.hermes, noop);
+      var hermesOpts = hermes.hermesSingletonFactory.firstCall.args[0];
+      assert.deepEqual(hermesOpts, {
+        hostname: 'localhost',
+        port: 5672,
+        username: 'guest',
+        password: 'guest',
+        queues: [ 'test-queue-01', 'test-queue-02' ]
+      });
     });
     it('should require a list of queues', function () {
       assert.throws(function () {
