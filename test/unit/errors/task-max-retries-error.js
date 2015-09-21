ee68a75e55031cfd4a150f8d5689b4059f69936c
Bryan Kendall <bryan@runnable.com>
2015-09-20 21:18:22 -0700

    tests: a little more comprehensive coverage for specific things

--- test/unit/errors/task-max-retries-error.js
+++ test/unit/errors/task-max-retries-error.js
@@ -8,8 +8,15 @@ var TaskMaxRetriesError = require('../../../lib/errors/task-max-retries-error');
 
 describe('TaskMaxRetriesError', function () {
   it('should default the message', function () {
-    var err = new TaskMaxRetriesError('a-queue', { hello: 'world' });
+    var err = new TaskMaxRetriesError('a-queue');
     assert.instanceOf(err, TaskMaxRetriesError);
     assert.match(err.message, /max retries reached/);
+  });
+  it('should set custom data on the error', function () {
+    var err = new TaskMaxRetriesError('a-queue', { hello: 'world' });
+    assert.deepEqual(err.data, {
+      queue: 'a-queue',
+      hello: 'world'
+    });
   });
 });
