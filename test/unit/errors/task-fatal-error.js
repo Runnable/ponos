a973de4040f67190600aa31ccfd295ef8f578454
Bryan Kendall <bryan@runnable.com>
2016-05-08 11:42:27 -0700

    transform task errors to error-cat

--- test/unit/errors/task-fatal-error.js
+++ test/unit/errors/task-fatal-error.js
@@ -1,25 +1,41 @@
 'use strict'
 
 const chai = require('chai')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
 
 const assert = chai.assert
 
-const TaskError = require('../../../lib/errors/task-error')
 const TaskFatalError = require('../../../lib/errors/task-fatal-error')
 
 describe('TaskFatalError', () => {
-  it('should inherit from Error and TaskError', () => {
+  const mockJob = { foo: 'bar' }
+
+  it('should inherit from error-cat~WorkerStopError', () => {
     const err = new TaskFatalError()
     assert.instanceOf(err, TaskFatalError)
-    assert.instanceOf(err, TaskError)
-    assert.instanceOf(err, Error)
+    assert.instanceOf(err, WorkerStopError)
+  })
+
+  it('should set the message correctly and set data', () => {
+    const err = new TaskFatalError('a-queue', 'had an error', mockJob)
+    assert.equal(err.message, 'a-queue: had an error')
+  })
+
+  it('should set the data correctly', () => {
+    const err = new TaskFatalError('a-queue', 'had an error', mockJob)
+    assert.deepEqual(err.data, { queue: 'a-queue', job: mockJob })
   })
 
   it('should set custom data on the error', () => {
-    const err = new TaskFatalError('a-queue', 'had an error', { hello: 'world' })
-    assert.equal(err.message, 'a-queue: had an error')
+    const err = new TaskFatalError(
+      'a-queue',
+      'had an error',
+      mockJob,
+      { hello: 'world' }
+    )
     assert.deepEqual(err.data, {
       queue: 'a-queue',
+      job: mockJob,
       hello: 'world'
     })
   })
