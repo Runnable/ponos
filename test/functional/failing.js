435528b8cb734241db346d7a0a537e0e0e8aebcb
Bryan Kendall <bryan@runnable.com>
2016-05-09 13:36:49 -0700

    fix all documentation to remove Task*Errors

--- test/functional/failing.js
+++ test/functional/failing.js
@@ -17,7 +17,7 @@ const _Worker = require('../../lib/worker')
 
 /*
  *  In this example, we are going to pass an invalid job to the worker that will
- *  throw a TaskFatalError, acknowledge the job, and not run it a second time.
+ *  throw a WorkerStopError, acknowledge the job, and not run it a second time.
  */
 describe('Basic Failing Task', () => {
   let server
@@ -68,7 +68,7 @@ describe('Basic Failing Task', () => {
         assert.ok(_Worker.prototype.run.calledOnce, '.run called once')
         /*
          *  We can get the promise and assure that it was fulfilled!
-         *  This should be _fulfilled_ because it threw a TaskFatalError and
+         *  This should be _fulfilled_ because it threw a WorkerStopError and
          *  acknowledged that the task was completed (even though the task
          *  rejected with an error)
          */
