44fb5681539c3d7380723e00431eb88f0be2ccb0
Anandkumar Patel <anand@runnable.com>
2016-09-06 14:27:40 -0700

    add integration test

---
+++ test/integration/rate-limiters/redis.js
@@ -0,0 +1,71 @@
+'use strict'
+const chai = require('chai')
+const Promise = require('bluebird')
+const put = require('101/put')
+const redis = require('redis')
+const sinon = require('sinon')
+
+const logger = require('../../../src/logger')
+const RedisRateLimiter = require('../../../src/rate-limiters/redis')
+
+const assert = chai.assert
+
+const redisConfig = {
+  port: '6379',
+  host: 'localhost'
+}
+describe('rate-limiters/redis integration test', () => {
+  let redisRateLimiter
+  const testClient = redis.createClient(redisConfig.port, redisConfig.host)
+
+  beforeEach((done) => {
+    redisRateLimiter = new RedisRateLimiter(put(redisConfig, {
+      log: logger.child({ module: 'ponos:test' })
+    }))
+    sinon.spy(Promise, 'delay')
+    redisRateLimiter.connect()
+      .then(() => {
+        testClient.flushall(done)
+      })
+  })
+
+  afterEach(() => {
+    Promise.delay.restore()
+  })
+
+  it('should immediately resolve', () => {
+    return assert.isFulfilled(redisRateLimiter.limit('test'))
+  })
+
+  it('should only call 1 in rate limit period', () => {
+    let count = 0
+    const tasks = []
+    for (var i = 0; i < 5; i++) {
+      tasks.push(redisRateLimiter.limit('test', {
+        maxOperations: 1,
+        durationMs: 500
+      }).then(() => { count++ }))
+    }
+    return Promise.all(tasks)
+    .timeout(50)
+    .catch(Promise.TimeoutError, () => {
+      assert.equal(count, 1)
+    })
+  })
+
+  it('should limit to 5 during period', () => {
+    let count = 0
+    const tasks = []
+    for (var i = 0; i < 10; i++) {
+      tasks.push(redisRateLimiter.limit('test', {
+        maxOperations: 5,
+        durationMs: 500
+      }).then(() => { count++ }))
+    }
+    return Promise.all(tasks)
+    .timeout(800)
+    .catch(Promise.TimeoutError, () => {
+      assert.equal(count, 5)
+    })
+  })
+}) // end rate-limiters/redis integration test
