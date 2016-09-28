b2f33f259dc53bc2f73ed253a15357ddea165de8
Anton Podviaznikov <podviaznikov@gmail.com>
2016-09-28 16:19:59 -0700

    add missing files

---
+++ test/functional/fixtures/worker-one.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const EventEmitter = require('events')
+const Promise = require('bluebird')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
+
+let rabbitmq
+/**
+ * A simple worker that will publish a message to a queue.
+ * @param {object} job Object describing the job.
+ * @param {string} job.queue Queue on which the message will be published.
+ * @returns {promise} Resolved when the message is put on the queue.
+ */
+module.exports = (job, jobMeta) => {
+  return Promise.resolve()
+    .then(() => {
+      if (!job.eventName) {
+        throw new WorkerStopError('eventName is required')
+      }
+      if (!job.message) {
+        throw new WorkerStopError('fail test message is required')
+      }
+    })
+    .then(() => {
+      const job = {
+        eventName: 'task',
+        message: 'hello world2'
+      }
+      rabbitmq.publishTask('ponos-test:two', job)
+      module.exports.emitter.emit(job.eventName, job, jobMeta)
+    })
+}
+
+module.exports.setPublisher = (publisher) => {
+  rabbitmq = publisher
+}
+
+module.exports.emitter = new EventEmitter()
---
+++ test/functional/fixtures/worker-two.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const EventEmitter = require('events')
+const Promise = require('bluebird')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
+
+/**
+ * A simple worker that will publish a message to a queue.
+ * @param {object} job Object describing the job.
+ * @param {string} job.queue Queue on which the message will be published.
+ * @returns {promise} Resolved when the message is put on the queue.
+ */
+module.exports = (job, jobMeta) => {
+  return Promise.resolve()
+    .then(() => {
+      if (!job.eventName) {
+        throw new WorkerStopError('eventName is required')
+      }
+      if (!job.message) {
+        throw new WorkerStopError('fail test message is required')
+      }
+    })
+    .then(() => {
+      module.exports.emitter.emit(job.eventName, job, jobMeta)
+    })
+}
+
+module.exports.emitter = new EventEmitter()
